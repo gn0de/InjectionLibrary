e6d551ac3958513860b9d807d2a5a68db426bcfc
Julius M. Hawkins <gn0de@yahoo.com>
2017-06-28 16:01:28 -0700

    Add files via upload

---
+++ InjectionLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright © Jason Larke 2012")]
+[assembly: AssemblyDescription("Generic DLL Injection Library")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("InjectionLibrary")]
+[assembly: AssemblyTitle("InjectionLibrary")]
+[assembly: AssemblyTrademark("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("bf78f76f-0d22-461f-a17d-53b3da231819")]
